dc73b05656365261ce4eb2e8a1cd58cf39c83f7e
Poonam <poonam.khandagale@softmass.co.in>
2025-01-06 17:29:00 +0530

    basics of react

--- 01basicactive/src/main.jsx
+++ 01basicactive/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+const reactElement = React.createElement(
+    'a',
+    { href: 'https://google.com', target: "_blank" },
+    'click to visit google'
+)
 createRoot(document.getElementById('root')).render(
-  
+    //   reactElement
     <App />
-   
+
 )
