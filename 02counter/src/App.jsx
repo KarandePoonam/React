b443db4adcef3d6c01b354bdfd89ed3458958227
Poonam <poonam.khandagale@softmass.co.in>
2025-01-07 16:31:10 +0530

    Counter project changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,12 +5,14 @@ function App() {
 
   const [counter, setCounter] = useState(15)
   const addValue = () => {
+    if(counter<25)
     setCounter(counter + 1)
     // setCounter((prevCounter => prevCounter + 1))
     // setCounter((prevCounter => prevCounter + 1))
 
   }
   const removeValue = () => {
+    if(counter>0)
     setCounter(counter - 1)
   }
 
